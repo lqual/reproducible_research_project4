51779eed2bc86a3f5fd08370995fa1596a55644b
Lucas Qualmann <qualmannl@gmail.com>
2019-09-16 09:01:46 -0500

    plot2 done

--- plot2.R
+++ plot2.R
@@ -2,15 +2,19 @@
 
 plot2data <- data2 %>% mutate(year = year(BGN_DATE)) %>% 
         filter(year >= 1993) %>%
-        select(year, EVTYPE, people_harmed, FATALITIES) 
+        select(year, EVTYPE, people_harmed) 
 plot2data <- plot2data %>% group_by(EVTYPE) %>% 
-        summarize(harm = sum(people_harmed), fatal = sum(FATALITIES)) %>% 
-        arrange(desc(harm))
+        summarize(harm = sum(people_harmed)) %>% arrange(desc(harm))
 plot2data <- plot2data[1:10, ]
-
 xplot2 <- plot2data %>% pull(EVTYPE)
 yplot2 <- plot2data %>% pull(harm)
-yfatal <- plot2data %>% pull(fatal)
-plot(1:10, yplot2, xaxt = "n")
-axis(1, at = 1:10, labels = xplot2)
-points(yfatal)
+barplot(yplot2, names.arg = xplot2, ylim = c(0, 25000), ylab = "People", 
+        main = "Total Injuries and Fatalities from Weather Events (1993-2011)", 
+        col = "blue", xlab = "Weather Event")
+
+
+
+
+
+
+
